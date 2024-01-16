177521d6e7c15615a65ae50e1de1f023a70017cc
Yuya Isaka <zxwg6329@gmail.com>
2024-01-16 15:45:48 +0900

    add kata

--- test.hs
+++ test.hs
@@ -5,27 +5,36 @@ quadruple :: (Num a) => a -> a
 quadruple x = double (double x)
 
 -- Factorial of a positive integer:
+factorial :: (Num a, Enum a) => a -> a
 factorial n = product [1 .. n]
 
 -- Average of a list of integers:
+average :: (Foldable t) => t Int -> Int
 average ns = sum ns `div` length ns
 
+a :: Integer
 a = b + c
   where
     b = 1
     c = 2
 
+d :: Integer
 d = a * 2
 
+n :: Int
 n = a `div` length xs
   where
     a = 10
     xs = [1, 2, 3, 4, 5]
 
+lastyuya :: [a] -> a
 lastyuya xs = xs !! (length xs - 1)
 
+lastyuya2 :: [a] -> a
 lastyuya2 xs = head (reverse xs)
 
+inityuya :: [a] -> [a]
 inityuya xs = take (length xs - 1) xs
 
+inityuya2 :: [a] -> [a]
 inityuya2 xs = reverse (tail (reverse xs))
